65e317a0f4d292e4f39c3fe635aa9f44ac67b990
jon-neher <62190461+jon-neher@users.noreply.github.com>
2025-07-01 14:37:31 -0600

    chore: prepare for Lovable deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean,
-  ),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
@@ -19,26 +12,14 @@ export default defineConfig(({ mode }) => ({
   build: {
     minify: mode === "production" ? "esbuild" : false,
     sourcemap: false,
-    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks: {
           "react-vendor": ["react", "react-dom"],
           "auth-vendor": ["@clerk/clerk-react"],
           "db-vendor": ["@supabase/supabase-js"],
-          "query-vendor": ["@tanstack/react-query"],
         },
       },
     },
-  },
-  define: {
-    "process.env.NODE_ENV": JSON.stringify(mode),
-  },
-  optimizeDeps: {
-    include: [
-      "@clerk/clerk-react",
-      "@supabase/supabase-js",
-      "@tanstack/react-query",
-    ],
   },
 }));
